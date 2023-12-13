7e5af31049395e165a8532f7f623013fc75f5742
Tolga Uzun <tolgauzun@gmail.com>
2023-12-13 17:46:44 -0500

    Add config attributes to code

--- src/mqtt.py
+++ src/mqtt.py
@@ -4,11 +4,6 @@ from typing_extensions import Self
 
 from typing import Any, Mapping, Optional
 
-
-
-
-
-
 from viam.module.types import Reconfigurable
 from viam.proto.app.robot import ComponentConfig
 from viam.proto.common import ResourceName, Vector3
@@ -24,52 +19,28 @@ import asyncio
 import paho.mqtt.client as mqtt
 from viam.components.component_base import ValueTypes
 
-
-
 LOGGER = getLogger(__name__)
 
 
-
 class mqtt_sensor(Sensor, Reconfigurable):
-    
-    """
-    Sensor represents a physical sensing device that can provide measurement readings.
-    """
-    
-
     MODEL: ClassVar[Model] = Model(ModelFamily("tuzumkuru", "sensor"), "mqtt")
     
     # create any class parameters here, 'some_pin' is used as an example (change/add as needed)
-    some_pin: int
+    broker_address: str
+    broker_port: int
+    mqtt_topic: str
 
-    def  __init__(self, name, *args):
-        print("__init__ called")
-        self.name = name
-        # Set your MQTT broker's information
-        self.broker_address = "test.mosquitto.org"
-        self.port = 1883
-        self.topic = "my_test_topic"
-        self.latest_reading = None
+    latest_reading: str
 
-        # Create an MQTT client instance
-        self.client = mqtt.Client()
-
-        # Set up callbacks
-        self.client.on_connect = self.on_connect
-        self.client.on_message = self.on_message
-
-        # Connect to the broker
-        self.client.connect(self.broker_address, self.port, 60)
-
-        # Start the MQTT loop
-        self.client.loop_start()
-        
+    mqtt_client: mqtt.Client
+    mqtt_client = mqtt.Client()
+    
     # Constructor
     @classmethod
     def new(cls, config: ComponentConfig, dependencies: Mapping[ResourceName, ResourceBase]) -> Self:
-        my_class = cls(config.name)
-        my_class.reconfigure(config, dependencies)
-        return my_class
+        sensor = cls(config.name)
+        sensor.reconfigure(config, dependencies)
+        return sensor
 
     # Validates JSON Configuration
     @classmethod
@@ -82,35 +53,40 @@ class mqtt_sensor(Sensor, Reconfigurable):
 
     # Handles attribute reconfiguration
     def reconfigure(self, config: ComponentConfig, dependencies: Mapping[ResourceName, ResourceBase]):
-        # here we initialize the resource instance, the following is just an example and should be updated as needed
-        self.some_pin = int(config.attributes.fields["some_pin"].number_value)
+        self.latest_reading = None
+
+        # Set your MQTT broker's information
+        self.broker_address = config.attributes.fields['broker_address'].string_value
+        self.broker_port = int(config.attributes.fields['broker_port'].number_value)
+        self.mqtt_topic = config.attributes.fields['mqtt_topic'].string_value
+
+        
+        # Set up callbacks
+        self.mqtt_client.on_connect = self.on_connect
+        self.mqtt_client.on_message = self.on_message
+
+        # Connect to the broker
+        self.mqtt_client.connect(self.broker_address, self.broker_port, 60)
+
+        # Start the MQTT loop
+        self.mqtt_client.loop_start()
+
         return
 
-    """ Implement the methods the Viam RDK defines for the Sensor API (rdk:component:sensor) """
-    def on_connect(self, client, userdata, flags, rc):
-        print("Connected with result code "+str(rc))
 
+    def on_connect(self, client, userdata, flags, rc):
+        LOGGER.info("Connected with result code "+str(rc))
         # Subscribe to the specified topic when connected
-        client.subscribe(self.topic)
+        client.subscribe(self.mqtt_topic)
+
 
     def on_message(self, client, userdata, msg):
-        print("on_message called")
+        LOGGER.info(f"MQTT Message Received: {str(msg)}")
         # Update the latest reading when a new message is received
         self.latest_reading = msg.payload.decode('utf-8')
- 
-        print(self.latest_reading)
     
-    async def get_readings(
-        self, *, extra: Optional[Mapping[str, Any]] = None, timeout: Optional[float] = None, **kwargs
-    ) -> Mapping[str, Any]:
-        """
-        Obtain the measurements/data specific to this sensor.
 
-        Returns:
-            Mapping[str, Any]: The measurements. Can be of any type.
-        """
-        ...
-         # Replace mqtt_data with self.latest_reading in the following code
+    async def get_readings(self, *, extra: Optional[Mapping[str, Any]] = None, timeout: Optional[float] = None, **kwargs) -> Mapping[str, Any]:
         if self.latest_reading is None:
             return {"status": "No data"}
 
@@ -133,17 +109,21 @@ class mqtt_sensor(Sensor, Reconfigurable):
 
         # For any other type, convert it to a string and return
         return {"data": str(self.latest_reading)}
-
-
-
     
-    async def do_command(self, command: Mapping[str, ValueTypes], *, timeout: Optional[float] = None, **kwargs) -> Mapping[str, ValueTypes]:
-        print("Done!")
 
+    async def do_command(self, command: Mapping[str, ValueTypes], *, timeout: Optional[float] = None, **kwargs) -> Mapping[str, ValueTypes]:
+        LOGGER.info("Test do_command: DONE!")
 
 
 async def main():
+
+    config = ComponentConfig()
+    config.attributes.fields['broker_address'].string_value = "test.mosquitto.org"
+    config.attributes.fields['broker_port'].number_value = 1883
+    config.attributes.fields['mqtt_topic'].string_value = "my_test_topic"
+
     my_mqtt_sensor=mqtt_sensor(name="mqtt-sensor")
+    my_mqtt_sensor.reconfigure(config,None)
     time.sleep(2)
     signal = await my_mqtt_sensor.get_readings()
     print(signal)
