db2e92878396342dec91098016ee17927c4c1aed
Tolga Uzun <tolgauzun@gmail.com>
2023-12-13 21:15:02 -0500

    Update checks on config parameters

--- src/mqtt.py
+++ src/mqtt.py
@@ -25,15 +25,29 @@ LOGGER = getLogger(__name__)
 class mqtt_sensor(Sensor, Reconfigurable):
     MODEL: ClassVar[Model] = Model(ModelFamily("tuzumkuru", "sensor"), "mqtt")
     
-    # create any class parameters here, 'some_pin' is used as an example (change/add as needed)
+    # Broker parameters
     broker_address: str
     broker_port: int
     mqtt_topic: str
+    mqtt_qos: int = 0
 
     latest_reading: str
 
+    #Client
     mqtt_client: mqtt.Client
     mqtt_client = mqtt.Client()
+
+    # Client Parameters
+    # Get default values of parameters    
+    client_id: str = mqtt_client._client_id  
+    clean_session : bool = mqtt_client._clean_session
+    protocol : int = mqtt_client._protocol # MQTTv31 = 3 MQTTv311 = 4 MQTTv5 = 5
+    transport : str = mqtt_client._transport
+
+    # enable_logger(logger=None) might be used to let paho use viam logger
+
+    # username_pw_set(username, password=None)
+    # set username and password if provided
     
     # Constructor
     @classmethod
@@ -55,12 +69,23 @@ class mqtt_sensor(Sensor, Reconfigurable):
     def reconfigure(self, config: ComponentConfig, dependencies: Mapping[ResourceName, ResourceBase]):
         self.latest_reading = None
 
-        # Set your MQTT broker's information
+        # Get parameters from config
         self.broker_address = config.attributes.fields['broker_address'].string_value
         self.broker_port = int(config.attributes.fields['broker_port'].number_value)
         self.mqtt_topic = config.attributes.fields['mqtt_topic'].string_value
         
+        
+        # Set optional parameters if set in config
+        self.mqtt_qos = int(config.attributes.fields['mqtt_qos'].number_value)
+        self.client_id = config.attributes.fields['client_id'].string_value if 'client_id' in config.attributes.fields else self.client_id
+        self.clean_session = bool(config.attributes.fields['clean_session'].bool_value)  if 'clean_session' in config.attributes.fields else self.clean_session
+        self.protocol= config.attributes.fields['protocol'].string_value if 'protocol' in config.attributes.fields else self.protocol # MQTTv31 = 3 MQTTv311 = 4 MQTTv5 = 5
+        self.transport = config.attributes.fields['transport'].string_value if 'transport' in config.attributes.fields else self.transport # tcp or websockets
+
+        self.mqtt_client.reinitialise(self.client_id, self.clean_session)
+
         # Set up callbacks
+        
         self.mqtt_client.on_connect = self.on_connect
         self.mqtt_client.on_message = self.on_message
 
@@ -69,14 +94,13 @@ class mqtt_sensor(Sensor, Reconfigurable):
 
         # Start the MQTT loop
         self.mqtt_client.loop_start()
-
         return
 
 
     def on_connect(self, client, userdata, flags, rc):
         LOGGER.info("Connected with result code "+str(rc))
         # Subscribe to the specified topic when connected
-        client.subscribe(self.mqtt_topic)
+        client.subscribe(self.mqtt_topic, qos=self.mqtt_qos)
 
 
     def on_message(self, client, userdata, msg):
@@ -115,14 +139,20 @@ class mqtt_sensor(Sensor, Reconfigurable):
 
 
 async def main():
-
+    # Create an example ComponentConfig
     config = ComponentConfig()
     config.attributes.fields['broker_address'].string_value = "test.mosquitto.org"
     config.attributes.fields['broker_port'].number_value = 1883
     config.attributes.fields['mqtt_topic'].string_value = "my_test_topic"
+    config.attributes.fields['clean_session'].bool_value = False
+    config.attributes.fields['transport'].string_value = "websockets"
+    config.attributes.fields['mqtt_qos'].number_value = 0
 
+    # Create a sensor to test code
     my_mqtt_sensor=mqtt_sensor(name="mqtt-sensor")
+    my_mqtt_sensor.validate(config)
     my_mqtt_sensor.reconfigure(config,None)
+
     time.sleep(2)
     signal = await my_mqtt_sensor.get_readings()
     print(signal)
